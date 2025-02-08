80c495e76152ba1ce649be9e8ca81071cf065f5e
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-08 23:35:55 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -54,3 +54,12 @@ CROSS JOIN Weather today
 
 WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
     AND today.temperature > yesterday.temperature
+
+
+    //leetcodeSELECT today.id
+FROM Weather yesterday 
+CROSS JOIN Weather today
+
+WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature
+;
