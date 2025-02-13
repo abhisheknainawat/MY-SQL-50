e99a03aa27eb605ba3a97e28d2cc9956e3d44429
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-13 23:39:19 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -88,3 +88,13 @@ GROUP BY S.student_id, S.student_name, SU.subject_name
 ORDER BY S.student_id, S.student_name, SU.subject_name
 ;
 //
+leetcode 570
+
+SELECT E1.name
+FROM Employee E1
+JOIN (
+    SELECT managerId, COUNT(*) AS directReports
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) E2 ON E1.id = E2.managerId;
