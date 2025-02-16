5a56893d9b2265f4405c778ce3c50809ceaddf65
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-16 22:58:43 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -112,3 +112,11 @@ SELECT id , movie , description , rating
 FROM cinema 
 WHERE ID%2=1 && description != 'boring'
 ORDER BY rating DESC
+
+//leetcode 1251 
+// average selling price
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id
