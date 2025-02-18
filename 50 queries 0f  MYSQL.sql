3d8ff14f9bea54f9055c9195e3271f0f94a5d88e
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-18 22:04:34 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -141,3 +141,7 @@ ORDER BY percentage DESC, contest_id;
 
 //leetcode 1211 
 // queries quality and percentage 
+
+select query_name,round((sum(rating/position)/count(rating)),2) as quality, round((sum(rating<3)/(count(*)))*100,2) as poor_query_percentage
+from Queries
+group by query_name;
