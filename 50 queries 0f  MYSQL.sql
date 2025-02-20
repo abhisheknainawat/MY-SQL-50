8835ce2ca75827bb71051aa1973d62bae0c6cb40
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-20 22:14:58 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -145,3 +145,4 @@ ORDER BY percentage DESC, contest_id;
 select query_name,round((sum(rating/position)/count(rating)),2) as quality, round((sum(rating<3)/(count(*)))*100,2) as poor_query_percentage
 from Queries
 group by query_name;
+//
