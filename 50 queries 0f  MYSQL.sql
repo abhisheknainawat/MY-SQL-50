8c545e2e67be2f5f51d7821dcb9ae140d674d8c6
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-23 23:43:00 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -158,3 +158,12 @@ select class
 from Courses
 group by class
 having count(class)>=5
+
+//leetcode 1070
+# Write your MySQL query statement below
+select product_id, year as first_year, quantity, price 
+from sales where (product_id, year) in (
+    select product_id, min(year)
+    from sales
+    group by product_id
+)
