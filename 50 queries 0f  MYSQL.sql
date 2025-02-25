49fa32fa1b303a63f76f57ba2e7e44421cd0dd7e
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-02-25 23:57:58 +0530

    first commit

--- 50 queries 0f  MYSQL.sql
+++ 50 queries 0f  MYSQL.sql
@@ -167,3 +167,11 @@ from sales where (product_id, year) in (
     from sales
     group by product_id
 )
+//1731
+
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
+
